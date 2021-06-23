dd08055f1e7f0d06058efa0b8824df755195579c
jjflorez <jhonjairofv4@gmail.com>
2021-06-22 21:10:47 -0500

    navbar create

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import {BarraDeMenu} from '../src/components/NavBar.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        
+        <BarraDeMenu />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+export function BarraDeMenu(){
+return (
+    <div>
+		<nav class=" navbar navbar-expand-sm bg-dark navbar-dark ">
+			<a class="navbar-brand" href="index.js">
+			<img src="../Imagenes/Logo.png" alt="logo" />
+			</a>
+			<ul class="navbar-nav" >
+			<li class="nav-item" >
+			<a class="nav-link" href="#">Simuladores </a>
+			</li>
+
+			<li class="nav-item" >
+			<a class="nav-link" id="simuladores" href="index.html"></a>
+			</li>
+			</ul>
+			</nav>
+	</div>
+
+)
+
+}
